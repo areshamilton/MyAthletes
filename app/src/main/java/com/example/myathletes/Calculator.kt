0fc7e7f431b9bc2d03b9725bcd494e3dc7523c9c
Jon <53204551+jjdorman@users.noreply.github.com>
2021-09-23 14:20:21 -0700

    Create Calculator.kt

---
+++ app/src/main/java/com/example/myathletes/Calculator.kt
@@ -0,0 +1,11 @@
+package com.example.myathletes
+
+// The calculator class will analyze a user's workout data and determine if the workout needs to be adjusted
+
+abstract class Calculator () {
+  
+  fun calcWorkout () {
+    
+  }
+  
+}
