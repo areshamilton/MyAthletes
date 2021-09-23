3feb5b4fbb926a09ab3a185effa071e1a28ec288
Ares <areshamilton@csu.fullerton.edu>
2021-09-22 19:40:42 -0700

    gave Video class properties and a display method

--- app/src/main/java/com/example/myathletes/video.kt
+++ app/src/main/java/com/example/myathletes/video.kt
@@ -1,5 +1,13 @@
 package com.example.myathletes
 
-class Video {
-    // add in code
+// Video class contains the name of the workout, a description of the workout, and a video demonstration
+// TODO: use YouTube Player API to embed the video demonstration
+class Video(val workout: String, val description: String, val link: String) {
+
+    // method that displays the Video object's properties
+    fun displayInfo() {
+        println("$workout")
+        println("$description")
+        println("$link")
+    }
 }
