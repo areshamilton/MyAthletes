646f8f24befe688186e17d052a1dd064a56104dd
eddierayo-156 <79113907+eddierayo-156@users.noreply.github.com>
2021-10-07 13:49:54 -0700

    had to reimplement the timer because it was giving me a android linking error. added a progress bar base that will be linked to the timer once implemeneted

--- app/src/main/java/com/example/myathletes/Timer.kt
+++ app/src/main/java/com/example/myathletes/Timer.kt
@@ -2,4 +2,33 @@ package com.example.myathletes
 
 class Timer {
     // Will add a timer that can be configured
+
+    // enum to track the timer current state
+    enum class TimerState{
+        Stopped, Paused, Running, Rest
+    }
+
+    private val timer = Timer()
+
+    fun startTimer(){
+        // TODO: when start button is clicked, timer progress bar will countdown
+    }
+
+    fun onPause(){
+        // TODO: when button is clicked, timer and progress will stop. future option to use rest time
+    }
+
+    fun onResume(){
+        // TODO: when button is clicked, time will resume and if rest time is running, it will paused as well
+    }
+
+    // once we can save and load workout plans, we will pass in the dataset
+    // and it will be outputted in the fragment timer
+    // use class activity intersection list as the base
+
+    // in .xml place style="@style/Widget.MaterialProgressBar.ProgressBar" in the
+    // progress bar section to hide it until code is implemented
+
+
+
 }
