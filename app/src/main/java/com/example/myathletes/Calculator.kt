80622caa40bdbe2d620678551a83b13991c93eb9
Jon <53204551+jjdorman@users.noreply.github.com>
2021-10-06 22:21:39 -0700

    Update Calculator.kt

--- app/src/main/java/com/example/myathletes/Calculator.kt
+++ app/src/main/java/com/example/myathletes/Calculator.kt
@@ -2,9 +2,15 @@ package com.example.myathletes
 
 // The calculator class will analyze a user's workout data and determine if the workout needs to be adjusted
 
+const val effortThreshold = 0.00
+
 abstract class Calculator () {
   
   fun calcWorkout () {
+    
+  }
+  
+  fun workoutEval (workExerted: Int, weightAttempted: Int): Int {
     
   }
   
