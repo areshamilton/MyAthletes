8bd63b05a57ef188f2b0f442e1304ebd9ceb2192
Ares <areshamilton@csu.fullerton.edu>
2021-10-06 21:19:57 -0700

    added a demonstration of how each workout's info would be displayed (image in place of video for now)

--- app/src/main/java/com/example/myathletes/MainActivity.kt
+++ app/src/main/java/com/example/myathletes/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.myathletes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // USING THIS OBJECT TO TEST DISPLAY FOR WORKOUTS
+        val exampleVideo: Video = Video(workout = "Bench Press", description = "used to train an " +
+                "athlete's upper body as they push weights while lying on their back", link = "bench_press")
+
+        // displays the name of the workout
+        var workoutDisplay: TextView = findViewById(R.id.workout)
+        workoutDisplay.text = exampleVideo.workout
+
+        // displays the description of the work
+        var descriptionDisplay: TextView = findViewById(R.id.description)
+        descriptionDisplay.text = exampleVideo.description
+
+        // displays a photo of the workout
+        // NOTE: using an image instead of a view until we learn how to use APIs
+        // for now, the link is just the name of an image file
+        var photoDisplay: ImageView = findViewById(R.id.photo)
+        photoDisplay.setImageResource(R.drawable.bench_press)
+
+
+        // TODO: implement data binding for cleaner code
+        // TODO: figure out how to fix the margins
+        // TODO: make everything look better
+
     }
 }
