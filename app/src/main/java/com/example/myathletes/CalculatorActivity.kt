b9345b83e15eb484e20ccffb719ee85e866d80c0
jjdorman <jjdorman@csu.fullerton.edu>
2021-10-21 12:22:04 -0700

    Enabling navigation

--- app/src/main/java/com/example/myathletes/CalculatorActivity.kt
+++ app/src/main/java/com/example/myathletes/CalculatorActivity.kt
@@ -2,13 +2,10 @@ package com.example.myathletes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class CalculatorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.calculator)
     }
-
 }
