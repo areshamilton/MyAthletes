eac34147ff54d44497fd96dbec48ae6d0fb67935
Jon <53204551+jjdorman@users.noreply.github.com>
2021-11-18 16:16:28 -0800

    Update MainActivity.kt

--- app/src/main/java/com/example/myathletes/MainActivity.kt
+++ app/src/main/java/com/example/myathletes/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    //Should be unneccessary after merge
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
