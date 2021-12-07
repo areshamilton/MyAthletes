b4444b2e254f2b7c9a9f5f70b85a810aade663f6
jluong13 <62159801+jluong13@users.noreply.github.com>
2021-12-07 13:22:26 -0800

    Credentials only goes to the main menu if entering root, root for username/password.

--- app/src/main/java/com/example/myathletes/Signup.kt
+++ app/src/main/java/com/example/myathletes/Signup.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import com.example.myathletes.database.SignupDatabase
 import com.example.myathletes.databinding.CredentialsBinding
 import com.example.myathletes.databinding.SignupBinding
 
@@ -21,6 +23,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Signup : Fragment() {
+    private lateinit var binding: SignupBinding
 
 
     override fun onCreateView(
@@ -31,12 +34,22 @@ class Signup : Fragment() {
         val binding: SignupBinding =
             DataBindingUtil.inflate(inflater, R.layout.signup,container, false)
 
-        binding.signup.setOnClickListener(){ view: View ->
+        val application = requireActivity().application
+        val dataSource = SignupDatabase.getInstance(application).signupDao
+        val viewModelFactory = SignupViewModelFactory(dataSource, application)
+        val signupViewModel =
+            ViewModelProvider(
+                this, viewModelFactory).get(SignupViewModel::class.java)
+        // Connect the IntersectionViewModel with the variable in the layout
+        binding.signupViewModel = signupViewModel
+        // Assign the lifecycle owner to the activity so it manages the data accordingly.
+        binding.lifecycleOwner = this
+
+        binding.back.setOnClickListener(){ view: View ->
             view.findNavController()
                 .navigate(R.id.action_signup2_to_credentials)
         }
         return binding.root
     }
-
 
 }
